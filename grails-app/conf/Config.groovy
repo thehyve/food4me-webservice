053d3c90f2cc1050f5ec8d6fca46ee0423cacb12
Joris Borgdorff <joris@thehyve.nl>
2018-05-08 16:27:59 +0200

    Updated build conf

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -2,10 +2,9 @@
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
 
- grails.config.locations = [ "classpath:${appName}-config.properties",
-                             "classpath:${appName}-config.groovy",
-                             "file:${userHome}/.grails/${appName}-config.properties",
-                             "file:${userHome}/.grails/${appName}-config.groovy"]
+ grails.config.locations = [ "file:/var/lib/tomcat8/conf/Catalina/localhost/${appName}-config.groovy",
+                             "file:${userHome}/.grails/${appName}-config.groovy",
+                             "classpath:${appName}-config.groovy"]
 
  if (System.properties["${appName}.config.location"]) {
     grails.config.locations << "file:" + System.properties["${appName}.config.location"]
